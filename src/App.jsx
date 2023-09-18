b2ca9a0b12d31b8079cc0eb2ac4faeec2d88d19a
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-09-18 11:21:44 +0300

    connected redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+
 import { Header } from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
+import { store } from "./redux/store";
 
 import "./scss/app.scss";
-import { useState } from "react";
 
 export const SearchContext = React.createContext();
 
